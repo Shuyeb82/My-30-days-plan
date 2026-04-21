24f0013adb281afd4f8aa9019c0f6fd3b7023f7b
Shuyeb82 <it23047@mbstu.ac.bd>
2026-04-21 22:51:23 +0600

    Add files via upload

---
+++ Day-1/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+  Car myCar=new Car();
+  myCar.color="Red";
+  myCar.model="Toyota";
+  myCar.year=2022;
+  myCar.displayInfo();
+    }
+}
---
+++ Day-1/main .java
@@ -0,0 +1,11 @@
+public class main{
+    static void main(String[] args){
+        Student s1=new Student();
+        s1.name="Shuyaib Hasan";
+        s1.Roll=IT23047;
+        s1.district="Jamalpur";
+        s1.Number="017******87";
+        s1.Show();
+       
+    }
+}
